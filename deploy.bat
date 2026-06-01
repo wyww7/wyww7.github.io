d3d1af5dcb7f427bb6eb2637b073824b541e0278
wyww <1513275624@qq.com>
2026-06-01 21:00:28 +0800

    site update: 2026/06/01 周一 21:00:28.90

--- deploy.bat
+++ deploy.bat
@@ -1,8 +1,6 @@
 @echo off
-
-:: Deploy script for Hugo blog - GitHub Pages
 set HUGO_PATH=C:\software\other\hugo\hugo_extended_withdeploy_0.161.1_windows-amd64
-
+set REPO_URL=https://github.com/wyww7/wyww7.github.io
 cd /d "%~dp0"
 
 echo =======================================
@@ -13,7 +11,7 @@ git commit -m "site update: %date% %time%"
 git push origin main
 
 echo =======================================
-echo  2. Clean old cache & generate site...
+echo  2. Clean old cache and generate site...
 echo =======================================
 "%HUGO_PATH%\hugo.exe" --gc --cleanDestinationDir
 
@@ -21,12 +19,14 @@ echo =======================================
 echo  3. Deploy to gh-pages branch...
 echo =======================================
 cd public
+git init
+git remote add origin %REPO_URL%
 git add -A
 git commit -m "deploy: %date% %time%"
-git push origin gh-pages --force
+git push -f origin HEAD:gh-pages
 cd ..
 
 echo =======================================
-echo  Done! Source backed up & site deployed.
+echo  Done! Source backed up and site deployed.
 echo =======================================
 pause
